556ec6e86ea2af56f9901598e894385de18d4ee6
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-03-23 01:30:42 +0300

    Assignment Completed

---
+++ lib/migrations/versions/b4cfb074b418_rename_email_to_email_address.py
@@ -0,0 +1,24 @@
+"""Rename email to email_address
+
+Revision ID: b4cfb074b418
+Revises: 791279dd0760
+Create Date: 2024-03-23 01:29:01.036020
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4cfb074b418'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
